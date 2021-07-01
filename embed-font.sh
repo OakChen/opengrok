965ca58a022e3613b168567117b17d78344d8372
Oak Chen <oak@sfysoft.com>
2021-07-01 09:01:24 +0800

    使用Liberation Mono作为首选等宽字体，以}来分隔替换，以免出现字体替换超预期

--- embed-font.sh
+++ embed-font.sh
@@ -3,12 +3,13 @@ replace_font() {
 	if [[ -f /tmp/embed-font.tmp && -f "$1" && ! -f "$1.bak" ]]; then
 		cp $1 $1.bak
 		cat /tmp/embed-font.tmp $1.bak > $1
-		sed -i "s/font-family:\([^?]*\)monospace/font-family: \"JetBrains Mono\", \"Roboto Mono\",\1monospace/g" $1
-		sed -i "s/font-family:monospace/font-family:\"JetBrains Mono\",\"Roboto Mono\",monospace/g" $1
-		sed -i "s/font-family:\([^?]*\)sans-serif/font-family: Roboto,\1sans-serif/g" $1
-		sed -i "s/font-family:sans-serif/font-family:Roboto,sans-serif/g" $1
-		sed -i "s/ pre {/ pre {\n    font-size: 90%;\n    font-family: \"JetBrains Mono\", \"Roboto Mono\", \"Courier New\", monospace;/g" $1
-		sed -i "s/ pre{/ pre{font-size:90%;font-family:\"JetBrains Mono\",\"Roboto Mono\",\"Courier New\",monospace;/g" $1
+		sed -i 's#font-family:\([^}]*\)monospace#font-family: "Liberation Mono", "JetBrains Mono", "Roboto Mono", "Courier New", \1monospace#g' $1
+		sed -i 's#font-family:monospace#font-family:"Liberation Mono","JetBrains Mono","Roboto Mono","Courier New",monospace#g' $1
+		sed -i 's#font-family:\([^}]*\)sans-serif#font-family: Roboto,\1sans-serif#g' $1
+		sed -i 's#font-family:sans-serif#font-family:Roboto,sans-serif#g' $1
+		sed -i 's# pre {# pre {\n    font-size: 90%;\n    font-family: "Liberation Mono", "JetBrains Mono", "Roboto Mono", "Courier New", monospace;#g' $1
+		sed -i 's# pre{# pre{font-size:90%;font-family:"Liberation Mono","JetBrains Mono","Roboto Mono","Courier New",monospace;#g' $1
+		sed -i 's#.con{font-size:small}#.con{font-size:small;font-family:"Liberation Mono","JetBrains Mono","Roboto Mono","Courier New",monospace;}#g' $1
 	fi
 }
 
